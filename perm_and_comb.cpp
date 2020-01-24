ce0cb5fc176d51ac63f436fa0866875aac0f4379
kkr1947 <59726644+kkr1947@users.noreply.github.com>
2020-01-24 16:00:04 +0530

    Kuldeep Kumar 19HCS4028 PERMUTATON AND COMBINATION using RECURSION

--- perm_and_comb.cpp
+++ perm_and_comb.cpp
@@ -1,17 +1,47 @@
+/*
+Kuldeep Kumar
+19HCS4028
+C++ PROGRAM TO COMPUTE PERMUTATON AND COMBINATION BY RECURSION
+*/
 #include<iostream>
 using namespace std;
-
 int permutation(int n, int r)
 {
-  return 0;
+	if(r==0)
+  return 1;
+  else 
+  return n*permutation((n - 1),(r-1));
 }
-
 int combination(int n, int r)
 {
-  return 0;
+	if(r==0)
+  return 1;
+  else
+  return (n*combination((n - 1),(r-1)))/r;
 }
 
 int main()
 {
-  return 0;
+	int n,r;
+	int p,c;
+	cout<<"Enter two numbers"<<endl;
+	cin>>n;
+	cin>>r;
+	if(n<0||r<0||n<r)
+	{
+	cout<<"INVALID NUMBERS "<<endl;
+  	return 0;
+	}
+	else if(r==0)
+	{
+		p=1;
+		c=1;
+	}
+	else
+	{
+	p=permutation(n,r);
+	c=combination(n,(n-r));
+}
+	cout<<"The permutation of ("<<n<<","<<r<<") is = "<<p<<endl;
+	cout<<"The combination of ("<<n<<","<<r<<") is = "<<c<<endl;
 }
